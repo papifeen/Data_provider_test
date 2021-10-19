633ee04cc54a28dabf9fa0a9759aa8b44d6c5fa5
selenium <selenium@Rafeens-MacBook-Pro.local>
2021-10-18 20:53:48 -0400

    added SearchPage and initiialized driver

---
+++ src/main/java/Pages/SearchPage.java
@@ -0,0 +1,11 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SearchPage {
+WebDriver driver = new ChromeDriver();
+
+
+    }
+
