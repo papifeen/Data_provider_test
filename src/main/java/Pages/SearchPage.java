aa91e682e5f31487fd61f98b35e87e87205c3970
papifeen <rafeen.u2000@gmail.com>
2021-10-18 20:58:38 -0400

    Made changes to driver initialization

--- src/main/java/Pages/SearchPage.java
+++ src/main/java/Pages/SearchPage.java
@@ -3,8 +3,20 @@ package Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class SearchPage {
-WebDriver driver = new ChromeDriver();
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/selenium/Downloads/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize(); //maximize window
+        driver.manage().deleteAllCookies(); //delete cookies
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("https://www.google.ca/");
+    }
 
 
     }
